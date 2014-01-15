edb75cc5cb118cbc29cbfd97fbb42cb010e5720c
Atsushi Yoshida <Atsushi@Atsushi-no-MacBook-Pro-2.local>
2014-01-15 18:34:37 +0900

    Initial Commit

---
+++ Calcurator/CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAppDelegate.h
+//  Calcurator
+//
+//  Created by Atsushi Yoshida on 2014/01/15.
+//  Copyright (c) 2014年 Atsushi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calcurator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calcurator
+//
+//  Created by Atsushi Yoshida on 2014/01/15.
+//  Copyright (c) 2014年 Atsushi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAppDelegate class]));
+    }
+}
